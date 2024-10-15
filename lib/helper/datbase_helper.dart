fa4c2bc722d5a4216bc06f730cc0229f12af3bfc
Anees <aneesdahot2020@gmail.com>
2024-10-15 12:07:14 +0500

    created database helper

---
+++ lib/helper/datbase_helper.dart
@@ -0,0 +1,41 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static Database? _db;
+  static final DatabaseHelper instance = DatabaseHelper._();
+
+  DatabaseHelper._();
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDatabase();
+    return _db!;
+  }
+
+ Future<Database> _initDatabase() async {
+    final databaseDirPath = await getDatabasesPath();
+    final databasePath = join(databaseDirPath, 'chatty.db');
+    return await databaseFactory.openDatabase(
+      databasePath,
+      options: OpenDatabaseOptions(
+        version: 1,
+        onCreate: (db, version) async {
+          await _createTables(db);
+        },
+      ),
+    );
+  }
+
+  Future<void> _createTables(Database db) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT NOT NULL,
+        timestamp TEXT NOT NULL
+      )
+    ''');
+  }
+
+  
+}
