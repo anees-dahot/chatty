9355b18f36a5ebc96c2d4c8c7d4d4981fcfa06d7
Anees <aneesdahot2020@gmail.com>
2024-10-16 10:31:24 +0500

    created chat model

---
+++ lib/model/chat_model.dart
@@ -0,0 +1,30 @@
+class Message {
+  final int? id;
+  final String content;
+  final String isByMe;
+  final DateTime timestamp;
+
+  Message(
+      {this.id,
+      required this.content,
+      required this.isByMe,
+      required this.timestamp});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'content': content,
+      'isByMe': isByMe,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    return Message(
+      id: map['id'],
+      content: map['content'],
+      isByMe: map['isByMe'],
+      timestamp: DateTime.parse(map['timestamp']),
+    );
+  }
+}
